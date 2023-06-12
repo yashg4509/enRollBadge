ed83cd04c6d6a286cfe0c8921aae0bbf74315afe
yashg4509 <bludgermoonstone66@gmail.com>
2023-06-11 21:23:39 -0700

    changes fonts

--- frontend/my-app/src/dashboard/index.js
+++ frontend/my-app/src/dashboard/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App2 from "./App2";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
--- frontend/my-app/src/index.js
+++ frontend/my-app/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App2 from "./App";
+import 'typeface-inter';
+import "./fonts.css";
+
+
 
 ReactDOM.render(
   <React.StrictMode>
