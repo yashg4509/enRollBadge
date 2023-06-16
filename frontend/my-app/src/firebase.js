2fa4cbdc6e3c03c32cfafe55c5be2694a37c29c4
Yash Gupta <ysgupta@wisc.edu>
2023-06-16 08:39:32 -0700

    Update firebase.js

--- frontend/my-app/src/firebase.js
+++ frontend/my-app/src/firebase.js
@@ -11,15 +11,6 @@ const firebaseConfig = {
   appId: process.env.APP_ID
 }
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDDYcmTrSmsQ9IN3hwISXclB6mmKRlhbDc",
-//   authDomain: "uw-madison-course-notification.firebaseapp.com",
-//   projectId: "uw-madison-course-notification",
-//   storageBucket: "uw-madison-course-notification.appspot.com",
-//   messagingSenderId: "72175685541",
-//   appId: "1:72175685541:web:e277f1dbf72b423ece224c"
-// };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
